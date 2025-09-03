db2e0e7df9aa8fb7cf2903f047e750b550ec052f
eriick-monteiro <erick.m.anjos@gmail.com>
2025-09-03 16:28:08 -0300

    :sparkles: feat Avançar e Retroceder Músicas

--- script.js
+++ script.js
@@ -1,31 +1,98 @@
 const songName = document.getElementById("song-name");
 const bandName = document.getElementById("band-name");
 const song = document.getElementById("audio");
+const cover = document.getElementById("cover");
 const play = document.getElementById("play");
-const capa = document.getElementById("cover");
+const previous = document.getElementById("previous");
+const next = document.getElementById("next");
 
-musics = ["CyberScript", "CyberStyleSheet", "h3ll0-w0rld", "ReactThis"];
-music = musics[2];
+const cyberScript = {
+    songName: "CyberScript",
+    bandName: "N!gh7 ₿ØyZ",
+    file: "CyberScript"
+}
 
-songName.innerText = music;
-bandName.innerText = "N!gh7 ₿ØyZ";
-song.src = `songs/${music}.mp3`;
-capa.src = `images/${music}.webp`;
+const cyberStyleSheet = {
+    songName: "CyberStyleSheet",
+    bandName: "N!gh7 ₿ØyZ",
+    file: "CyberStyleSheet"
+}
+
+const helloWorld = {
+    songName: "h3ll0-w0rld",
+    bandName: "N!gh7 ₿ØyZ",
+    file: "h3ll0-w0rld"
+}
+
+const reactThis = {
+    songName: "ReactThis",
+    bandName: "N!gh7 ₿ØyZ",
+    file: "ReactThis"
+}
+
+playlist = [cyberScript, cyberStyleSheet, helloWorld, reactThis];
+let index = 0;
 let isPlaying = false;
 let isLiked = false;
 
-function playPause() {
-    if (!isPlaying) {
-        play.querySelector(".bi").classList.remove("bi-play-circle-fill");
-        play.querySelector(".bi").classList.add("bi-pause-circle-fill");
-        song.play();
-    } else if (isPlaying) {
-        play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
-        play.querySelector(".bi").classList.add("bi-play-circle-fill");
-        song.pause();
-    }
-    isPlaying = !isPlaying;
+function playSong() {
+    play.querySelector(".bi").classList.remove("bi-play-circle-fill");
+    play.querySelector(".bi").classList.add("bi-pause-circle-fill");
+    song.play();
+    isPlaying = true;
 }
 
+function pauseSong() {
+    play.querySelector(".bi").classList.remove("bi-pause-circle-fill");
+    play.querySelector(".bi").classList.add("bi-play-circle-fill");
+    song.pause();
+    isPlaying = false;
+}
+
+function playPause() {
+    if (isPlaying) {
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+initializeSong = () => {
+    cover.src = `images/${playlist[index].file}.webp`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].bandName;
+}
+
+previousSong = () => {
+    if (index == 0) {
+        index = playlist.length -1 ;
+    }
+    else {
+        index = index - 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+
+nextSong = () => {
+    if (index === playlist.length - 1) {
+        index = 0;
+    }
+    else {
+        index += 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+initializeSong();
+
 play.addEventListener("click", playPause);
+previous.addEventListener("click", previousSong);
+next.addEventListener("click", nextSong);
+
+
 
